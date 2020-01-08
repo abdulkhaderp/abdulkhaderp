5da4f952263e6eeb8ef5524e2aaf67c795b6c8cb
Abdul khader P <abdulkhaderp@gmail.com>
2020-01-08 14:23:26 +0530

    Add files via upload

---
+++ myapp/precache-manifest.95d9cf2b67b3624381f6a459dac868cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25469477e1bec26046bf7ee8bf90af6",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "e6c8a86b93c505ff99f6",
+    "url": "/myapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8739b7f88af07e4dae1e",
+    "url": "/myapp/static/css/main.5e5be183.chunk.css"
+  },
+  {
+    "revision": "e6c8a86b93c505ff99f6",
+    "url": "/myapp/static/js/2.f215bd6c.chunk.js"
+  },
+  {
+    "revision": "454cbbf41ef494c57342da4695ec7ed0",
+    "url": "/myapp/static/js/2.f215bd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8739b7f88af07e4dae1e",
+    "url": "/myapp/static/js/main.be3fbef9.chunk.js"
+  },
+  {
+    "revision": "44bc0d12c6c9e24897ad",
+    "url": "/myapp/static/js/runtime-main.3d12a942.js"
+  },
+  {
+    "revision": "992fe12fb73c9ceb88b2af0a932cad3b",
+    "url": "/myapp/static/media/NT_Logo.992fe12f.png"
+  },
+  {
+    "revision": "894e6941010f40a7e88b980fb2beb7c0",
+    "url": "/myapp/static/media/nt-strategy.894e6941.jpg"
+  }
+]);
