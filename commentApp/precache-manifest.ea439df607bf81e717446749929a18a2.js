b045d72672948a4e6c4f8412179857ff6c360679
Abdul khader P <abdulkhaderp@gmail.com>
2020-03-14 22:39:37 +0530

    Add files via upload

---
+++ commentApp/precache-manifest.ea439df607bf81e717446749929a18a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9bea47d92376c3f404c290b35b7855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c9e48f31f42e97288d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4cc40c6cef738936548e",
+    "url": "/static/css/main.ea3970d4.chunk.css"
+  },
+  {
+    "revision": "b6c9e48f31f42e97288d",
+    "url": "/static/js/2.920cf44a.chunk.js"
+  },
+  {
+    "revision": "c2e34aef15b7aa9edf980156e72f2078",
+    "url": "/static/js/2.920cf44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc40c6cef738936548e",
+    "url": "/static/js/main.30aead86.chunk.js"
+  },
+  {
+    "revision": "8a29e129527b4dcae16f",
+    "url": "/static/js/runtime-main.57c9518f.js"
+  }
+]);
